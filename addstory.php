90e30efeb29ca835b16fee79a2ab5e3a8b02261c
Anton Gladyshev <returnstrike@yandex.ru>
2016-07-21 16:57:14 +0600

    minor changes

--- addstory.php
+++ addstory.php
@@ -10,4 +10,47 @@
 		</form>
 	</body>
  </html>
-
+ 
+ <?php
+	include("connection.php");
+	try {
+		$conn = new PDO("mysql: host=$servername;port=$port;dbname=$dbname", 
+			$username, $password);
+		//// set the PDO error mode to exception
+		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$STORY_VOTE_CNT = 0;
+		$STORY_ID = 0;
+		$STORY_TEXT = "BLah blah blah";
+		$file = $_FILES['image']['tmp_name'];
+		if(!isset($file))
+			echo "Please select image";
+		else
+		{
+			$STORY_IMAGE = addslashes(file_get_contents($_FILES['image']['tmp_name']));
+			$IMAGE_NAME  = addcslashes($_FILES['image']['name']);
+			echo $IMAGE_NAME;
+			$IMAGE_SIZE  = getimagesize($_FILES['image']['tmp_name']);
+			if ($IMAGE_SIZE==FALSE)
+				echo "That it is not an image";
+			else
+			{
+				$sql_read   = "SELECT * FROM STORIES";
+				$sql_ins_s  = "INSERT INTO STORIES (STORY_ID, STORY_TEXT, STORY_IMAGE, STORY_VOTE_CNT)
+					VALUES ('$STORY_ID', '$STORY_TEXT', '$STORY_IMAGE', '$STORY_VOTE_CNT')";
+				////use exec() because no results are returned
+				$conn->exec($sql_ins_s);
+				echo "New record created successfully";
+			}
+		}
+		
+		unset($username);
+		unset($password);
+		
+		
+	}
+	catch(PDOException $e){
+		echo $e->getMessage();
+	}
+	$conn = null;
+?>
+ 
--- connection - sample.php
+++ connection - sample.php
@@ -3,8 +3,5 @@ $servername  = "localhost";
 $port        = "3306";
 $username    = "username";
 $password    = "password";
-$trying_from = "from.me";
 $dbname      = "dbname";
-$site        = "http://test.this";
 ?>
-
